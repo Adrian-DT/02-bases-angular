eb6fe7f5b6294ce520174b7fb8a6f5b03b6b630c
Adrian Delgado <adriandt_work@outlook.com>
2025-08-13 19:01:46 +0200

    Uso de signal (señales, zoneless)

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -9,20 +9,35 @@ import { Component } from "@angular/core";
       width: 75px;
     }
   `,
+  // Para especificar que no quieres utilizar ZoneJS en este componente, mejorando su performance
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
 
   counter = 10;
+  counterSignal = signal(10);
+
+  constructor(){
+    setInterval(() => {
+      console.log('Tick');
+      // this.counter += 1;
+      this.counterSignal.update((value) => value + 1);
+    }, 2000);
+  }
 
   increaseBy(value: number) {
     this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value);
+    this.counterSignal.update(currentValue => currentValue + value);
   }
 
   decrementBy(value: number) {
     this.counter -= value;
+    this.counterSignal.update(currentValue => currentValue - value);
   }
 
   resetCounter(){
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
