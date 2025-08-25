f68504b70af96cd976bf0a04216c6725c4b2855c
Adrian Delgado <adriandt_work@outlook.com>
2025-08-25 17:32:59 +0200

    NgClass, utilizado para aplicar clases en función de condiciones en etiquetas html de nuestros componentes

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from "@angular/common";
 import { ChangeDetectionStrategy, Component, computed, signal } from "@angular/core";
 
 // Creamos la interfaz para especificar el tipo de dato que contendrá el array de characters
@@ -11,14 +12,25 @@ interface Character {
   templateUrl: './dragonball-page-component.html',
   // Para especificar que no quieres utilizar ZoneJS en este componente, mejorando su performance
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // Importamos el ngClass para poder utilizarlo en las etiquetas html
+  imports: [
+    // NgClass
+  ]
 })
 
 export class DragonballPageComponent {
   // Indicamos que characters es una señal de arrays de tipo Character
   characters = signal(<Character[]>[
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 2, name: 'Vegeta', power: 9000 },
     { id: 3, name: 'Piccolo', power: 3000 },
   ]);
+
+  // Funcion que retorna un objeto con la clase text-danger de bootstrap
+  // poweredClass = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //   }
+  // });
 
 }
