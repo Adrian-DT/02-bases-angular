7fad3e711b65e2a42ecb9028911b2bf0b46f52ee
Adrian Delgado <adriandt_work@outlook.com>
2025-09-07 09:39:29 +0200

    Cargar información del Local Storage

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,15 +1,24 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+// Función para cargar la información del LocalStorage de los Characters
+function loadFromLocalStorage(): Character[] {
+
+  const characters = localStorage.getItem('characters');
+
+  // Aquí sería conveniente comprobar que vayamos a retornar un array de tipo Characters por si ha sido modificado
+  // De no ser así, deberiamos lanzar algún tipo de error, ahora de manera rápida simplemente retornamos con
+  // el operador ternario, un array vacío.
+
+  return characters ? JSON.parse(characters) : [];
+}
+
 // Con provideIn indicamos la disponibilidad donde será injectable este servicio como dependencia
 @Injectable({providedIn: 'root'})
 export class DragonBallService {
 
-  // Indicamos que characters es una señal de arrays de tipo Character
-  characters = signal(<Character[]>[
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 9000 },
-  ]);
+  // Indicamos que characters es una señal de arrays de tipo Character y lo cargamos del LocalStorage
+  characters = signal(<Character[]>(loadFromLocalStorage()));
 
   addCharacter(character: Character) {
     this.characters.update(
