2661318943cb658c06580256bf76502e47178161
Adrian Delgado <adriandt_work@outlook.com>
2025-09-07 09:57:30 +0200

    HashStrategy, para que angular gestione las rutas desde /# al realizar un despliegue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // HashStrategy, se utiliza para obligar
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
